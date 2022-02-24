7fad22b26424b99095c1b67fede0f29f9ff03254
wanniDev <wannidev0928@gmail.com>
2022-02-24 23:56:06 +0900

    Feat:init

---
+++ src/main/java/me/action/caller/CallerApplication.java
@@ -0,0 +1,13 @@
+package me.action.caller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/action/caller/CallerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.action.caller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
